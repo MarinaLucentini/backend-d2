859f84977b6e42b836e1bc1f191444d043abe7cf
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-05-21 16:39:19 +0200

    es4

--- src/es3/Es3.java
+++ src/es3/Es3.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Es3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Scrivi una frase");
         String text = sc.nextLine();
 
 
@@ -16,7 +17,7 @@ public class Es3 {
             for (int i = 0; i < testo.length; i++) {
                 System.out.println(testo[i] + ",");
             }
-
+            System.out.println("Scrivi una frase");
             text = sc.nextLine();
         }
 
